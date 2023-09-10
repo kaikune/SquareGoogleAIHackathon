bda7ebfa228ab5c3835d7d99bac77e20c99878fd
kaikune <alexkai03@live.com>
2023-09-09 23:23:08 -0400

    Added boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Test from './components/test'
+
+function App() {
+  return (
+    // Can get rid of test for actual development
+    <Test />
+  );
+}
+
+export default App;
---
+++ client/src/components/test.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/testServer')
+            .then(res => res.json())
+            .then(users => {
+                console.log(users)
+                this.setState({ users: users })
+            });
+    }
+
+    render() {
+        return(
+            <div>
+            <h1>
+                hello world
+            </h1>
+            <ul> 
+                {
+                    this.state.users.forEach(user => {
+                        <li>
+                            {user.test}
+                        </li>
+                    }
+                    )
+                }
+            </ul>
+            </div>
+
+        );
+    }
+}
+
+export default Test;
