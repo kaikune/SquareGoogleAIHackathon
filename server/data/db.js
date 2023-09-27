5aa00041f028bfb6ac16619dbcf66772de971bef
Luke Roumy <Luke5r@icloud.com>
2023-09-27 16:00:09 -0400

    Added database connection and models

--- server/data/db.js
+++ server/data/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 const uri = "mongodb+srv://user0:user0pass@test1.zp8ibld.mongodb.net/?retryWrites=true&w=majority";
 
-
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB Atlas');
--- server/models/product.js
+++ server/models/product.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
 const ProductSchema = new Schema({
   name: {
     type: String,
