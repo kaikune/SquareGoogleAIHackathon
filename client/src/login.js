79e5b6cff3381d42f56cb9311bbf0c539e5dd6f7
ouckah <aidan.ouckama@gmail.com>
2023-10-12 23:25:13 -0400

    🚀 Login Page (FE ONLY)

---
+++ client/src/login.js
@@ -0,0 +1,32 @@
+import { React } from "react";
+import { Store } from 'lucide-react';
+
+function Login() {
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center bg-gray-200 w-screen h-screen">
+                
+                <div className="flex flex-col justify-evenly items-center w-3/4 md:w-1/2 h-3/4 bg-white rounded-2xl drop-shadow-xl">
+                    <div className="padding"/>
+                    <div className="flex flex-col justify-center items-center">
+                        <Store color="black" size={100} />
+                        <h1 className="text-black font-bold text-4xl uppercase tracking-wider select-none">Snap Cart</h1>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center w-full gap-4">
+                        <button className="bg-silver-300 w-3/4 px-8 py-4 rounded-full transition-all duration-300 hover:brightness-75">
+                            <h1 className="text-silver-500 font-bold text-2xl uppercase">Create</h1>
+                        </button>
+
+                        <button className="bg-silver-500 w-3/4 px-8 py-4 rounded-full transition-all duration-300 hover:brightness-75">
+                            <h1 className="text-white font-bold text-2xl uppercase">Log In</h1>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Login;
