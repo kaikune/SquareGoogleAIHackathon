31f5003dc2366db6f77eb05bf504df6a05103622
kaikune <alexkai03@live.com>
2023-10-15 00:37:08 -0400

    Small edits

--- client/src/apiconfig.js
+++ client/src/apiconfig.js
@@ -1,3 +1,3 @@
 // apiConfig.js
-export const BASE_URL = 'http://localhost:9000'; // Set your API base URL here
+export const BASE_URL = 'http://localhost:9000';
 //export const BASE_URL = 'https://server-qoogfebxba-uc.a.run.app';
