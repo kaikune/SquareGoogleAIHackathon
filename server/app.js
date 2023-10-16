1022641289af0837a647df51e8c25444f51ca182
kaikune <alexkai03@live.com>
2023-10-15 21:55:09 -0400

    Cleaned up app.js

--- server/app.js
+++ server/app.js
@@ -3,18 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-require('dotenv').config();
-
-var indexRouter = require('./routes/index');
-var testServerRouter = require('./routes/testServer');
-var createDataset = require('./routes/createDataset');
-var getSignedUrls = require('./routes/getSignedURLs');
-var trainModel = require('./routes/trainModel');
-var processPayment = require('./routes/processPayment');
-var getPrice = require('./routes/getPrice');
-var getStoreInfo = require('./routes/getStoreInfo');
-
 var app = express();
+var setupRoutes = require('./routes/index');
+require('dotenv').config();
 
 const corsOptions = {
     origin: true,
@@ -27,14 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// TODO: Cleanup like in 546 lecture
-app.use('/', indexRouter);
-app.use('/api/testServer', testServerRouter);
-app.use('/api/createDataset', createDataset);
-app.use('/api/getSignedUrls', getSignedUrls);
-app.use('/api/trainModel', trainModel);
-app.use('/api/processPayment', processPayment);
-app.use('/api/getPrice', getPrice);
-app.use('/api/getStoreInfo', getStoreInfo);
+// Binds request handlers to their respective routes
+setupRoutes(app);
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var createDataset = require('./routes/createDataset');
+var getSignedUrls = require('./routes/getSignedURLs');
+var trainModel = require('./routes/trainModel');
+var processPayment = require('./routes/processPayment');
+var getPrice = require('./routes/getPrice');
+var getStoreInfo = require('./routes/getStoreInfo');
+var loginAuth = require('./routes/loginAuth');
+var createUser = require('./routes/createUser');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const setupRoutes = (app) => {
+    app.use('/api/createDataset', createDataset);
+    app.use('/api/getSignedUrls', getSignedUrls);
+    app.use('/api/trainModel', trainModel);
+    app.use('/api/processPayment', processPayment);
+    app.use('/api/getPrice', getPrice);
+    app.use('/api/getStoreInfo', getStoreInfo);
+    app.use('/api/loginAuth', loginAuth);
+    app.use('/api/createUser', createUser);
 
-module.exports = router;
+    app.use('*', (req, res) => {
+        res.status(404).json({ error: 'Not found' });
+    });
+};
+
+export default setupRoutes;
