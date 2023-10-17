038fe4f3a2a842f3aad3580bd6107007ab1f411f
kaikune <alexkai03@live.com>
2023-10-17 10:04:40 -0400

    Added some error checking to items

--- client/src/items.js
+++ client/src/items.js
@@ -55,6 +55,11 @@ function Items() {
             setMessage('Please select one or more files.');
             return;
         }
+        if (label.length === 0) {
+            setMessage('Please enter a label');
+        }
+
+        setMessage('Preparing to upload files');
 
         const fileNames = files.map((file) => file.name); // Extract file names
 
@@ -75,6 +80,7 @@ function Items() {
                     setMessage(`File ${fileNames[i]} upload failed with status ${response.status}.`);
                 }
             }
+            setMessage('Files done uploading!');
         } catch (error) {
             setMessage(`File upload failed: ${error.message}`);
         }
@@ -86,15 +92,13 @@ function Items() {
             <div className="flex flex-col justify-center items-center w-2/3 h-1/3 bg-silver-300 border-dotted border-silver-500 border-8 rounded-full absolute top-20 pointer-events-none">
                 <Upload size={100} />
                 <h1 className="text-2xl font-bold">Upload Files</h1>
-                {
-                    files.length <= 4 ? (
-                        files.map((file) => (
-                            <h1>{file.name}</h1>
-                        ))
-                    ) : (
-                        <h1>{files[0].name} <br /> + {files.length - 1} more...</h1>
-                    )
-                }
+                {files.length <= 4 ? (
+                    files.map((file) => <h1>{file.name}</h1>)
+                ) : (
+                    <h1>
+                        {files[0].name} <br /> + {files.length - 1} more...
+                    </h1>
+                )}
             </div>
             <input className="bg-gray-200 w-1/3 p-5 rounded-full" type="text" value={label} placeholder="Pasta Sauce" onChange={handleLabelChange} />
             <input className="bg-gray-200 w-1/3 p-5 rounded-full" type="number" value={price} placeholder="1.99" onChange={handlePriceChange} />
