9ce2fafa09aca10b74ec5ff4d2f3bad5f09b25ab
kaikune <alexkai03@live.com>
2023-10-17 11:47:07 -0400

    Set prod as default server

--- client/src/apiconfig.js
+++ client/src/apiconfig.js
@@ -1,4 +1,4 @@
 // apiConfig.js
-export const BASE_URL = 'http://localhost:9000';
+//export const BASE_URL = 'http://localhost:9000';
 
-//export const BASE_URL = 'https://backend-qoogfebxba-uc.a.run.app';
+export const BASE_URL = 'https://backend-qoogfebxba-uc.a.run.app';
